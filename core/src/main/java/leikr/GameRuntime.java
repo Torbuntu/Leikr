f72c23dccea1dc37f7283b8727f3994415ab7a03
torbuntu <sorensentor@tuta.io>
2018-12-15 21:10:06 -0600

    -- GameRuntime now accepts a properties file for changing the runtime ex: groovy, lua, ruby etc...

--- core/src/main/java/leikr/GameRuntime.java
+++ core/src/main/java/leikr/GameRuntime.java
@@ -3,6 +3,8 @@ package leikr;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.mini2Dx.core.game.BasicGame;
@@ -11,8 +13,9 @@ import org.mini2Dx.miniscript.core.GameScriptingEngine;
 import org.mini2Dx.miniscript.core.ScriptBindings;
 import org.mini2Dx.miniscript.core.exception.InsufficientCompilersException;
 import org.mini2Dx.miniscript.groovy.GroovyGameScriptingEngine;
+import org.mini2Dx.miniscript.lua.LuaGameScriptingEngine;
 
-public class Leikr extends BasicGame {
+public class GameRuntime extends BasicGame {
 
     public static final String GAME_IDENTIFIER = "torbuntu.leikr";
 
@@ -26,14 +29,34 @@ public class Leikr extends BasicGame {
 
     final String appName;
 
-    public Leikr() {
+    public GameRuntime() {
         appName = "test";
         dir = new File("./Code/main.groovy");
     }
 
+    GameScriptingEngine getEngine() {
+        Properties prop = new Properties();
+        InputStream stream;
+        try {
+            stream = new FileInputStream(new File("./game.properties"));
+            prop.load(stream);
+            switch (prop.getProperty("runtime").toLowerCase()) {
+                case "lua":
+                    return new LuaGameScriptingEngine();
+                case "groovy":
+                default:
+                    return new GroovyGameScriptingEngine();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(GameRuntime.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // if all fails, return groovy scripting engine.
+        return new GroovyGameScriptingEngine();
+    }
+
     @Override
     public void initialise() {
-        scriptEngine = new GroovyGameScriptingEngine();
+        scriptEngine = getEngine();
         scriptBindings = new ScriptBindings();
         scriptBindings.put("game", engine);
         try {
@@ -42,7 +65,7 @@ public class Leikr extends BasicGame {
             engine = (LeikrEngine) scriptBindings.get("game");
             engine.init();
         } catch (InsufficientCompilersException | IOException ex) {
-            Logger.getLogger(Leikr.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GameRuntime.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
--- desktop/src/leikr/desktop/DesktopLauncher.java
+++ desktop/src/leikr/desktop/DesktopLauncher.java
@@ -1,20 +1,17 @@
 package leikr.desktop;
 
 import org.mini2Dx.desktop.DesktopMini2DxConfig;
-
 import com.badlogic.gdx.backends.lwjgl.DesktopMini2DxGame;
-
-import leikr.Leikr;
+import leikr.GameRuntime;
 
 public class DesktopLauncher {
 
     public static void main(String[] arg) {
-        DesktopMini2DxConfig config = new DesktopMini2DxConfig(Leikr.GAME_IDENTIFIER);
+        DesktopMini2DxConfig config = new DesktopMini2DxConfig(GameRuntime.GAME_IDENTIFIER);
         config.vSyncEnabled = true;
         config.title = "Leikr";
         config.width = 320;
         config.height = 240;
-        new DesktopMini2DxGame(new Leikr(), config);
-
+        new DesktopMini2DxGame(new GameRuntime(), config);
     }
 }
