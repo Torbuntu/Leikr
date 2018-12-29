cca7ed2a6ed0bc2d9dfcc7bfe257d3ed6983bca7
Torbuntu <sorensentor@tuta.io>
2018-12-28 18:32:27 -0600

    -- adding core methods to SpriteLoader

--- core/src/main/java/leikr/SpriteLoader.java
+++ core/src/main/java/leikr/SpriteLoader.java
@@ -5,10 +5,59 @@
  */
 package leikr;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import java.util.ArrayList;
+import org.mini2Dx.core.graphics.Sprite;
+import org.mini2Dx.core.graphics.TextureRegion;
+
 /**
  *
  * @author tor
  */
 public class SpriteLoader {
+
+    AssetManager assetManager;
+    TextureRegion[][] regions_0;
+    TextureRegion[][] regions_1;
+    ArrayList<Sprite> spriteBank;
+    Texture sheet_0;
+    Texture sheet_1;
+
+    SpriteLoader(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        spriteBank = new ArrayList<>();
+        loadSpriteSheets();
+        splitSprites();
+        addSpritesToSpriteBank();
+    }
+
+    private void loadSpriteSheets() {
+        sheet_0 = new Texture("./Games/" + MenuScreen.GAME_NAME + "/Sprites/Sprites_0.png");
+        sheet_1 = new Texture("./Games/" + MenuScreen.GAME_NAME + "/Sprites/Sprites_1.png");
+    }
+
+    private void splitSprites() {
+        regions_0 = (TextureRegion[][]) TextureRegion.split(sheet_0, 8, 8);
+        regions_1 = (TextureRegion[][]) TextureRegion.split(sheet_1, 8, 8);
+    }
+
+    private void addSpritesToSpriteBank() {
+        for (TextureRegion[] regionC : regions_0) {
+            for (TextureRegion regionR : regionC) {
+                spriteBank.add(new Sprite(regionR));                
+            }
+        }
+        for (TextureRegion[] regionC : regions_1) {
+            for (TextureRegion regionR : regionC) {
+                spriteBank.add(new Sprite(regionR));                
+            }
+        }
+    }
     
+    public ArrayList getSpriteBank(){
+        return spriteBank;
+    }
+    
+
 }
